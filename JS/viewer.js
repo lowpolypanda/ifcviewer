71daae520a9a1cd00499b68e9f98abcce30904d1
Low Poly Panda <llsunyer@gmail.com>
2022-07-03 10:59:18 +0200

    Update model.ifc path

--- JS/viewer.js
+++ JS/viewer.js
@@ -3,8 +3,7 @@ const urlParam = new URLSearchParams(window.location.search);
 const id = urlParam.get('id');
 const title = "/ " + projectArray[id].title;
 document.getElementById("title").innerText = title;
-//const model = projectArray[id].model;
-const model = `../PROJECTS/${id}/model.ifc`;
+const model = `./PROJECTS/${id}/model.ifc`;
 
 import { Color } from 'three';
 import { IfcViewerAPI } from "web-ifc-viewer";
